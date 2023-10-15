67fea174b0f4a026bcf8093e60a9f1c706598ac8
Ronald M <ronaldmh20@gmail.com>
2023-10-14 22:32:52 -0400

    feat > adding header  elements, classes and style file

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
+import { Autocomplete } from '@react-google-maps/api';
+import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+import useStyles from './styles';
 
 
-const Header = ({ }) => {
-  return(
-    <h1>Header</h1>
+const Header = ({ onPlaceChanged, onLoad }) => {
+  const classes = useStyles();
+
+  return (
+    <AppBar position="static">
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h5" className={classes.title}>
+          Map Advisor
+        </Typography>
+        <Box display="flex">
+          <Typography variant="h6" className={classes.title}>
+            Find a new place
+          </Typography>
+          {/*<Autocomplete>*/}
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase placeholder="Search…" classes={{ root: classes.inputRoot, input: classes.inputInput }} />
+            </div>
+          {/*</Autocomplete>*/}
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
-}
+};
 
 export default Header;
-
-
-  
