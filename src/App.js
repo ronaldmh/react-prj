7ef4c74630fb6872dea7cb757493816b19ca5db5
Ronald M <ronaldmh20@gmail.com>
2023-10-14 22:13:21 -0400

    Feat > creation full components structure - importing CssBaseline and Grid from material UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { useState, useEffect }  from 'react';
+import { CssBaseline, Grid } from '@material-ui/core';
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Carto from './components/Carto/Carto';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
 
-      <h1>React Final Project</h1>
+      <CssBaseline></CssBaseline>
+      <Header></Header>
+      <Grid container spacing={3} style={{width: '100%'}}>
+        <Grid item xs={12} md={4}>
+          <List></List>
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Carto></Carto>
+        </Grid>        
+      </Grid>     
       
     </div>
   );
---
+++ src/components/Carto/Carto.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Carto = ({ }) => {
+  return(
+    <h1>Map</h1>
+  );
+}
+
+export default Carto;
+
+
+  
