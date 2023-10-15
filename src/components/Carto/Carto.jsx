3e899bb955fc3151cdb58fa9c30cf9b821fe03a4
Ronald M <ronaldmh20@gmail.com>
2023-10-15 10:40:08 -0400

    feat > fetch google maps javascript API - Store API keys in environment variables file

--- src/components/Carto/Carto.jsx
+++ src/components/Carto/Carto.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab/Rating';
+
+import useStyles from './styles';
+
 
 
 const Carto = ({ }) => {
-  return(
-    <h1>Map</h1>
+
+  const matches = useMediaQuery('(min-width:600px)');
+  const classes = useStyles();
+  const coordinates = { lat: 11, lng: -74 };
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={9}
+        margin={[50, 50, 50, 50]}
+        options={''}
+        onChange={''}
+        onChildClick={''}
+      >
+      </GoogleMapReact>
+    </div>
   );
 }
 
