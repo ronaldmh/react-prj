38694b8130e6b290ed9a44de9dcb42bd069f57b7
Ronald M <ronaldmh20@gmail.com>
2023-10-16 18:23:44 -0400

    Feat > Adding places API index -  Adding use effect and use states to get coordinates from map in APP.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,32 @@ import Header from './components/Header/Header';
 import List from './components/List/List';
 import Carto from './components/Carto/Carto';
 import './App.css';
+import { getPlacesData } from './api';
 
 
 function App() {
+
+  const [places, setPlaces] = useState([]);
+  const [coordinates, setCoordinates] = useState({});
+  const [bounds, setBounds] = useState(null);
+
+  // use efect at the start
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
+      setCoords({ lat: latitude, lng: longitude });
+    });
+  }, []);
+  
+
+  // use effect to update map
+  useEffect(() => {
+    getPlacesData()
+      .then((data) => {        
+        setPlaces(data);
+      })
+  }, [coordinates, bounds]);
+
+
   return (
     <div className="App">
 
@@ -18,7 +41,11 @@ function App() {
         </Grid>
         
         <Grid item xs={12} md={8}>
-          <Carto></Carto>
+          <Carto
+            setCoordinates={setCoordinates}
+            setBounds={setBounds}
+            coordinates={Coordinates}>
+          </Carto>
         </Grid>        
       </Grid>     
       
--- src/components/Carto/Carto.jsx
+++ src/components/Carto/Carto.jsx
@@ -5,21 +5,24 @@ import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
 import Rating from '@material-ui/lab/Rating';
 import useStyles from './styles';
 
-const Carto = ({ }) => {
+const Carto = ({ setCoordinates, setBounds, coordinates }) => {
 
-  const matches = useMediaQuery('(min-width:600px)');
+  const mobileDevice = useMediaQuery('(min-width:600px)');
   const classes = useStyles();
-  const coordinates = { lat: 11, lng: -74 };
+  
   return (
     <div className={classes.mapContainer}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
+        bootstrapURLKeys={{ key: process.env.GOOGLE_MAPS_API_KEY }}
         defaultCenter={coordinates}
         center={coordinates}
         defaultZoom={9}
         margin={[50, 50, 50, 50]}
         options={''}
-        onChange={''}
+        onChange={(e => { 
+          setCoordinates ({ lat: e.center.lat, lng: e.center.lng });
+          setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw })
+        })}
         onChildClick={''}
       >
       </GoogleMapReact>
