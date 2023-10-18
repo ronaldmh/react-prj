2c3a4622c6dde82579b5a2b917cbb01fcb444f15
Ronald M <ronaldmh20@gmail.com>
2023-10-18 18:54:58 -0400

    Modify component header adding useEffect loading and placechanged on search element

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Autocomplete } from '@react-google-maps/api';
 import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
-import useStyles from './styles';
+import useStyles from './styles.js';
 
 const Header = ({ onPlaceChanged, onLoad }) => {
   const classes = useStyles();
@@ -17,14 +17,14 @@ const Header = ({ onPlaceChanged, onLoad }) => {
           <Typography variant="h6" className={classes.title}>
             Find a new place
           </Typography>
-          {/*<Autocomplete>*/}
+          <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
             <div className={classes.search}>
               <div className={classes.searchIcon}>
                 <SearchIcon />
               </div>
               <InputBase placeholder="Search…" classes={{ root: classes.inputRoot, input: classes.inputInput }} />
             </div>
-          {/*</Autocomplete>*/}
+          </Autocomplete>
         </Box>
       </Toolbar>
     </AppBar>
