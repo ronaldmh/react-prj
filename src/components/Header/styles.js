aa708199672d3220fd030c592ea2611d3bb859b0
Ronald M <ronaldmh20@gmail.com>
2023-10-18 18:51:24 -0400

    Modifying styles to the card and header component

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,12 +1,12 @@
 import { alpha, makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
-  
   title: {
     display: 'none', 
     [theme.breakpoints.up('sm')]: {
       display: 'block',
     color: 'orange',
+      
     },
   },
   search: {
@@ -23,7 +23,7 @@ export default makeStyles((theme) => ({
     padding: theme.spacing(0, 2), height: '100%', position: 'absolute', pointerEvents: 'none', display: 'flex', alignItems: 'center', justifyContent: 'center',
   },
   inputRoot: {
-    color: 'inherit',    
+    color: 'inherit',
   },
   inputInput: {
     padding: theme.spacing(1, 1, 1, 0), paddingLeft: `calc(1em + ${theme.spacing(4)}px)`, transition: theme.transitions.create('width'), width: '100%', [theme.breakpoints.up('md')]: { width: '20ch' },
